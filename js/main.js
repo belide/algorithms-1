e340e887b69b4b26eba0e1b732b2eceded4dcbff
Martin Kravchenko <kravmart@gmail.com>
2017-10-18 19:26:19 +0200

    Initial configuration

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
-// Maths Examples
+// Algorithms - Numbers only
 
+// Declare initial variables
 let input = document.querySelector("#main-input"),
 	output = document.querySelector(".output > p"),
-	bubbleSortB = document.querySelector("#bubble-sort");
+	bubbleSortB = document.querySelector("#bubble-sort"),
+	insertionSortB = document.querySelector("#insertion-sort"),
+	mergeSortB = document.querySelector("#merge-sort"),
+	quickSortB = document.querySelector("#quick-sort"),
+	radixSortB = document.querySelector("#radix-sort"),
+	binarySearchB = document.querySelector("#binary-search");
 
+// Setup buttons
 bubbleSortB.addEventListener("click", bubbleSort);
+insertionSortB.addEventListener("click", insertionSort);
+mergeSortB.addEventListener("click", mergeSort);
+quickSortB.addEventListener("click", quickSort);
+radixSortB.addEventListener("click", radixSort);
+binarySearchB.addEventListener("click", binarySearch);
 
-// Maths Examples - Bubble Sort (Numbers only)
+// Algorithms - Bubble Sort
 function bubbleSort() {
 	let array = input.value.split(" "),
 		length = array.length,
 		sorted = false,
 		pass = 1,
-		highlight = "highlightUnchanged",
+		highlight = "highlightGreen",
 		print = "Initial&nbsp; : &nbsp;&nbsp;&nbsp;";
 	array.forEach(function(e, index) {
 		array[index] = +e;
-		print += "<span class='highlightEnd'>&nbsp; " + array[index] + "&nbsp; </span>";
+		print += "<span class='highlightBlue'>&nbsp; " + array[index] + "&nbsp; </span>";
 	});
 	print += "<br><br>";
 
@@ -24,21 +36,21 @@ function bubbleSort() {
 		sorted = true;
 		for (let i = 0; i < length - 1; i++) {
 			print += "Pass " + pass + " : &nbsp;&nbsp;&nbsp;";
-			highlight = "highlightUnchanged";
+			highlight = "highlightGreen";
 			if (array[i] > array[i + 1]) {
 				sorted = false;
 				let temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
-				highlight = "highlightChanged";
+				highlight = "highlightRed";
 			}
 			array.forEach(function(e, index) {
 				if (index === i || index === i + 1) {
 					print += "<span class='" + highlight + "'>&nbsp; " + e + " &nbsp;</span>";
 				} else if (index > length - 1) {
-					print += "<span class='highlightFixed'>&nbsp; " + e + " &nbsp;</span>";
+					print += "<span class='highlightOrange'>&nbsp; " + e + " &nbsp;</span>";
 				} else {
-					print += "<span class='highlightNone'>&nbsp; " + e + " &nbsp;</span>";
+					print += "<span class='highlightGrey'>&nbsp; " + e + " &nbsp;</span>";
 				}
 			});
 			print += "<br>";
@@ -53,8 +65,23 @@ function bubbleSort() {
 
 	print += "Final &nbsp; : &nbsp;&nbsp;&nbsp;";
 	array.forEach(function(e) {
-		print += "<span class='highlightEnd'>&nbsp; " + e + " &nbsp;</span>";
+		print += "<span class='highlightBlue'>&nbsp; " + e + " &nbsp;</span>";
 	});
 	output.innerHTML = print;
 	input.value = array.join(" ");
 }
+
+// Algorithms - Insertion Sort
+function insertionSort() {}
+
+// Algorithms - Merge Sort
+function mergeSort() {}
+
+// Algorithms - Quick Sort
+function quickSort() {}
+
+// Algorithms - Radix Sort
+function radixSort() {}
+
+// Algorithms - Binary Search
+function binarySearch() {}
