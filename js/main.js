4b68d4d873da9fabe91654662a3b95ee95b687ab
Martin Kravchenko <kravmart@gmail.com>
2017-10-25 15:09:31 +0200

    Added insertion sort

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 // Declare initial variables
 let input = document.querySelector("#main-input"),
-	outputContainer = document.querySelector(".output-container"),
 	output = document.querySelector(".output"),
 	bubbleSortB = document.querySelector("#bubble-sort"),
 	insertionSortB = document.querySelector("#insertion-sort"),
@@ -25,7 +24,6 @@ function bubbleSort() {
 		length = array.length,
 		sorted = false,
 		pass = 1,
-		highlight = "highlightGreen",
 		print = "<p>Initial&nbsp; : &nbsp;&nbsp;&nbsp;";
 
 	// Check for NaN and initial print line
@@ -46,7 +44,7 @@ function bubbleSort() {
 		// Pass loop
 		for (let i = 0; i < length - 1; i++) {
 			print += "Pass " + pass + " : &nbsp;&nbsp;&nbsp;";
-			highlight = "highlightGreen";
+			let highlight = "highlightGreen";
 			if (array[i] > array[i + 1]) {
 				sorted = false;
 				let temp = array[i];
@@ -75,6 +73,7 @@ function bubbleSort() {
 		alert("Maximum number of passes (30) reached!\nThe list was not fully sorted.\r\nYou can use the algorithm again to sort the list further.");
 		output.innerHTML = "";
 	} else {
+		alert(`List sorted successfully.\nNumber of passes: ${pass - 1}.`)
 		print += "Final &nbsp; : &nbsp;&nbsp;&nbsp;";
 		array.forEach(function(e) {
 			print += "<span class='highlightBlue'>&nbsp; " + e + " &nbsp;</span>";
@@ -88,7 +87,56 @@ function bubbleSort() {
 
 // Algorithms - Insertion Sort
 function insertionSort() {
-	alert("Coming soon!");
+	let array = input.value.split(" "),
+		length = array.length,
+		print = "<p>Initial&nbsp; : &nbsp;&nbsp;&nbsp;";
+
+	// Check for NaN and initial print line
+	for (let i = 0; i < length; i++) {
+		if (isNaN(array[i])) {
+			alert("Only numbers are allowed.");
+			output.innerHTML = "";
+			return 1;
+		}
+		array[i] = +array[i];
+		print += "<span class='highlightBlue'>&nbsp; " + array[i] + "&nbsp; </span>";
+	}
+	print += "<br><br>";
+
+	// Main loop
+	for (let i = 1; i < length; i++) {
+		for (let k = i; k > 0; k--) {
+			print += "Index " + i + " : &nbsp;&nbsp;&nbsp;";
+			let highlight = "highlightGreen";
+			if (array[k] < array[k - 1]) {
+				highlight = "highlightRed";
+				let temp = array[k - 1];
+				array[k - 1] = array[k];
+				array[k] = temp;
+			}
+			array.forEach(function(e, index) {
+				if (index === k || index === k - 1) {
+					print += "<span class='" + highlight + "'>&nbsp; " + e + " &nbsp;</span>";
+				} else {
+					print += "<span class='highlightGrey'>&nbsp; " + e + " &nbsp;</span>";
+				}
+			});
+			print += "<br>";
+			if (highlight === 'highlightGreen') {
+				break;
+			}
+		}
+		print += "<br>";
+	}
+
+	alert("List sorted successfully.");
+	print += "Final &nbsp; : &nbsp;&nbsp;&nbsp;";
+	array.forEach(function(e) {
+		print += "<span class='highlightBlue'>&nbsp; " + e + " &nbsp;</span>";
+	});
+	print += "</p>";
+	output.innerHTML = print;
+	input.value = array.join(" ");
 }
 
 // Algorithms - Merge Sort
